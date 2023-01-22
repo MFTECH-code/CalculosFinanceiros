32bf51a7bc3cf3967998f645ab1461ae4c32141b
mftech <matheussilvafeitosa78@gmail.com>
2023-01-21 23:45:43 -0300

    Cálculo SAC sem nenhuma abstração de POO

--- CalculosFinanceiros/Program.cs
+++ CalculosFinanceiros/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var outstandingBalance = 120000.0;
+var installmentNumber = 10;
+var interestPercentage = 0.05;
+var installmentValue = 0.0;
+var amortizationValue = outstandingBalance / installmentNumber;
+
+for (int i = 0; i <= installmentNumber; i++)
+{
+    if (i == 0)
+        continue;
+
+    var installmentInterestValue = outstandingBalance * interestPercentage;
+    installmentValue = installmentInterestValue + amortizationValue;
+    outstandingBalance -= amortizationValue;
+    Console.WriteLine($"Parcela {i} - Valor: R${installmentValue.ToString("F2")}");
+    Console.WriteLine($"Juros: R${installmentInterestValue.ToString("F2")}");
+    Console.WriteLine("-----------------------------------------------------------");
+}
